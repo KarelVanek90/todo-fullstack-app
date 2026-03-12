f5b14e857c71c8209351fee42a6aa1f0a7dc673d
KarelVanek90 <vanek.karel001@gmail.com>
2026-03-12 17:40:58 +0100

    Fix React fallback regex for Render Node 22

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use("/api", todoRouters);
 
 app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.get("./*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
