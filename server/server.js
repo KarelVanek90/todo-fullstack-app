f2f539897b95f69427f8f91e367efb255ed3344c
KarelVanek90 <vanek.karel001@gmail.com>
2026-03-12 17:37:13 +0100

    Fix React fallback, add loading state, set Node 16 engine

--- server/server.js
+++ server/server.js
@@ -10,24 +10,20 @@ const PORT = process.env.PORT || 5000;
 
 const path = require("path");
 
-app.use(express.static(path.join(__dirname, "../client/build")));
-
-app.get("./*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-});
-
 app.use(cors());
 app.use(express.json());
 
 app.use("/api", autRouters);
 app.use("/api", todoRouters);
 
-connectDB();
+app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.get("/", (req, res) => {
-  res.send("Jsi na hlavni strance, vitej tedy");
+app.get("./*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
+connectDB();
+
 app.listen(PORT, () => {
-  console.log(`Server běží na http://localhost:${PORT}`);
+  console.log(`Server běží na portu ${PORT}`);
 });
