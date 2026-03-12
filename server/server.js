a07c9fc7b360360dc3f4bf8c66a77d6bff196eba
KarelVanek90 <157514917+KarelVanek90@users.noreply.github.com>
2026-03-12 16:22:38 +0100

    Update server.js
    .

--- server/server.js
+++ server/server.js
@@ -2,13 +2,6 @@ const express = require("express");
 const connectDB = require("./database/connect");
 const cors = require("cors");
 
-//const getData = require("./routes/todos/GET/getTodos");
-//const editData = require("./routes/todos/POST/editTodos");
-//const postDate = require("./routes/todos/POST/postTodos");
-//const deleteTodo = require("./routes/todos/DELETE/deleteTodo");
-//const register = require("./routes/auth/register");
-//const login = require("./routes/auth/login");
-
 const autRouters = require("./routes/authRoutes");
 const todoRouters = require("./routes/todoRoutes");
 
@@ -17,12 +10,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
-// app.use(postDate);
-// app.use(editData);
-//app.use(register);
-//app.use(login);
-//app.use(deleteTodo);
 
 app.use("/api", autRouters);
 app.use("/api", todoRouters);
